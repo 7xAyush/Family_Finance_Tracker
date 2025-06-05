40aa1ced0b47fbe76fb4997d23dd067cbe5359ea
Ayush Rai <ayush.kumarrai712@gmail.com>
2025-06-05 14:01:45 +0000

    Update BankMatching page to use rupee formatting
    cgen-1b32611130f7481f8c179a998a12d26e

--- src/pages/BankMatching.tsx
+++ src/pages/BankMatching.tsx
@@ -159,9 +159,9 @@ export function BankMatching() {
   };
 
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat("en-US", {
+    return new Intl.NumberFormat("en-IN", {
       style: "currency",
-      currency: "USD",
+      currency: "INR",
     }).format(amount);
   };
 
