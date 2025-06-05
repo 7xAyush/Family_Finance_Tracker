a07161423a944c5db811ef337a3a0c31eb15f6c5
Ayush Rai <ayush.kumarrai712@gmail.com>
2025-06-05 14:01:24 +0000

    Update TransactionList to use rupee formatting
    cgen-7ab0178c1a6a480c9e51300db81ec1a8

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -72,9 +72,9 @@ export function TransactionList({
   };
 
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat("en-US", {
+    return new Intl.NumberFormat("en-IN", {
       style: "currency",
-      currency: "USD",
+      currency: "INR",
     }).format(amount);
   };
 
